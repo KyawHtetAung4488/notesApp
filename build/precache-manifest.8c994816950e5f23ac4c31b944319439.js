02e030951a4fa95ed5739c7d90c1965008aa8339
Kyaw Htet Aung <kyawhtetaung@ucsy.edu.mm>
2020-10-02 15:25:22 +0630

    Added React Frontend App

---
+++ build/precache-manifest.8c994816950e5f23ac4c31b944319439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9b0ec8c692b4f953214cc129532b09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc661625f5f15d0060be",
+    "url": "/static/css/main.dea4be1d.chunk.css"
+  },
+  {
+    "revision": "bf8aadb2ec2d3b71fc5b",
+    "url": "/static/js/2.7356dfee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7356dfee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc661625f5f15d0060be",
+    "url": "/static/js/main.4c7a5d8c.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+
+
+app.use(express.static('build'))
+
 let notes = [  
     {    
         id: 1,    
